1934ca9f4e2c625932be92f9864170e636e68a77
future-highway <113635015+future-highway@users.noreply.github.com>
2022-10-16 12:41:29 -0400

    Refactor build.rs

--- build.rs
+++ build.rs
@@ -28,80 +28,147 @@
  *                                                                            *
 \* -------------------------------------------------------------------------- */
 
+use anyhow::Result;
 //use std::process::Command;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     // Example running "make command" during the build
     //Command::new("make").args(&["command"]).status().unwrap();
 
-    // gRPC
-    tonic_build::configure()
-.build_server(false)
-        .type_attribute(
-            "meta.AuraeMeta",
-            "#[allow(clippy::derive_partial_eq_without_eq)]",
-        )
-        .type_attribute(
-            "meta.ProcessMeta",
-            "#[allow(clippy::derive_partial_eq_without_eq)]",
-        )
-        .type_attribute(
-            "runtime.Executable",
-            "#[allow(clippy::derive_partial_eq_without_eq)]",
-        )
-        .type_attribute(
-            "runtime.ExecutableStatus",
-            "#[allow(clippy::derive_partial_eq_without_eq)]",
-        )
-        .type_attribute(
-            "meta.AuraeMeta",
-            "#[derive(::serde::Serialize, ::serde::Deserialize, ::macros::Output)]",
-        )
-        .type_attribute(
-            "meta.ProcessMeta",
-            "#[derive(::serde::Serialize, ::serde::Deserialize, ::macros::Output)]",
-        )
-        .type_attribute(
-            "observe.StatusRequest",
-            "#[derive(::serde::Serialize, ::serde::Deserialize, ::macros::Output)]",
-        )
-        .type_attribute(
-            "observe.StatusResponse",
-            "#[derive(::serde::Serialize, ::serde::Deserialize, ::macros::Output)]",
-        )
-        .type_attribute(
-            "schedule.ExecutableEnableResponse",
-            "#[derive(::serde::Serialize, ::serde::Deserialize, ::macros::Output)]",
-        )
-        .type_attribute(
-            "schedule.ExecutableDisableResponse",
-            "#[derive(::serde::Serialize, ::serde::Deserialize, ::macros::Output)]",
-        )
-        .type_attribute(
-            "schedule.ExecutableDestroyResponse",
-            "#[derive(::serde::Serialize, ::serde::Deserialize, ::macros::Output)]",
-        )
-        .type_attribute(
-            "runtime.Executable",
-            "#[derive(::serde::Serialize, ::serde::Deserialize, ::macros::Output, ::macros::Getters, ::macros::Setters)]",
-        )
-        .field_attribute(
-            "runtime.Executable.meta",
-            "#[getset(ignore)]",
-        )
-        .type_attribute(
-            "runtime.ExecutableStatus",
-            "#[derive(::serde::Serialize, ::serde::Deserialize, ::macros::Output)]",
-        )
-        .build_server(false)
-        .compile(
-            &[
-                "../auraed/stdlib/v0/meta.proto",
-                "../auraed/stdlib/v0/runtime.proto",
-                "../auraed/stdlib/v0/observe.proto",
-                "../auraed/stdlib/v0/schedule.proto",
-            ],
-            &["../auraed/stdlib/v0"],
-        )?;
+    generate_grpc_code()?;
+
     Ok(())
+}
+
+fn generate_grpc_code() -> Result<()> {
+    let mut tonic_builder = tonic_build::configure();
+
+    // Types generated from proto messages derive PartialEq without Eq. Add them here to suppress the warning.
+    for message in [
+        "meta.AuraeMeta",
+        "meta.ProcessMeta",
+        "runtime.Executable",
+        "runtime.ExecutableStatus",
+    ] {
+        tonic_builder = tonic_builder.type_attribute(
+            message,
+            "#[allow(clippy::derive_partial_eq_without_eq)]",
+        );
+    }
+
+    // Add proto messages here to add `#[derive(::serde::Serialize, ::serde::Deserialize)]` to the type.
+    // If other code generation (e.g., `::macros::Output`) needs these derive attributes, they will automatically be added.
+    let mut derive_serde_serialize_deserialize: Vec<&str> = vec![];
+
+    // Add proto messages here to generate output functions (e.g., `json()`) on the type
+    for message in [
+        "meta.AuraeMeta",
+        "meta.ProcessMeta",
+        "observe.StatusResponse",
+        "observe.StatusRequest",
+        "runtime.Executable",
+        "runtime.ExecutableStatus",
+        "schedule.ExecutableDestroyResponse",
+        "schedule.ExecutableDisableResponse",
+        "schedule.ExecutableEnableResponse",
+    ] {
+        if !derive_serde_serialize_deserialize.contains(&message) {
+            derive_serde_serialize_deserialize.push(message);
+        }
+
+        tonic_builder = tonic_builder
+            .type_attribute(message, "#[derive(::macros::Output)]");
+    }
+
+    // To generate getters and setters for message fields, see:
+    tonic_builder = generate_grpc_code_for_getters_setters(tonic_builder);
+
+    // Loop to add serde attributes
+    for message in derive_serde_serialize_deserialize {
+        tonic_builder = tonic_builder.type_attribute(
+            message,
+            "#[derive(::serde::Serialize, ::serde::Deserialize)]",
+        )
+    }
+
+    tonic_builder.build_server(false).compile(
+        &[
+            "../auraed/stdlib/v0/meta.proto",
+            "../auraed/stdlib/v0/runtime.proto",
+            "../auraed/stdlib/v0/observe.proto",
+            "../auraed/stdlib/v0/schedule.proto",
+        ],
+        &["../auraed/stdlib/v0"],
+    )?;
+
+    Ok(())
+}
+
+#[allow(clippy::single_element_loop)]
+fn generate_grpc_code_for_getters_setters(
+    mut tonic_builder: tonic_build::Builder,
+) -> tonic_build::Builder {
+    // Add proto messages here to generate getters and setters.
+    for getset in [GetSet {
+        message: "runtime.Executable",
+        ignore_fields: vec!["meta"],
+        ..Default::default()
+    }] {
+        let attribute = match (getset.getters, getset.setters) {
+            (true, true) => "#[derive(::macros::Getters, ::macros::Setters)]",
+            (true, false) => "#[derive(::macros::Getters)]",
+            (false, true) => "#[derive(::macros::Setters)]",
+            (false, false) => continue,
+        };
+
+        tonic_builder = tonic_builder.type_attribute(getset.message, attribute);
+
+        for field in getset.ignore_fields {
+            let path = format!("{}.{}", getset.message, field);
+            tonic_builder =
+                tonic_builder.field_attribute(path, "#[getset(ignore)]");
+        }
+
+        for field in getset.getters_ignore_fields {
+            let path = format!("{}.{}", getset.message, field);
+            tonic_builder =
+                tonic_builder.field_attribute(path, "#[getset(ignore_get)]");
+        }
+
+        for field in getset.setters_ignore_fields {
+            let path = format!("{}.{}", getset.message, field);
+            tonic_builder =
+                tonic_builder.field_attribute(path, "#[getset(ignore_set)]");
+        }
+    }
+
+    tonic_builder
+}
+
+struct GetSet {
+    /// The name of the proto message
+    message: &'static str,
+    /// Field names to ignore (no get or set function)
+    ignore_fields: Vec<&'static str>,
+    /// Should getters be generated
+    getters: bool,
+    /// Field names to ignore only when generating getters
+    getters_ignore_fields: Vec<&'static str>,
+    /// Should setters be generated
+    setters: bool,
+    /// Field names to ignore only when generating setters
+    setters_ignore_fields: Vec<&'static str>,
+}
+
+impl Default for GetSet {
+    fn default() -> Self {
+        Self {
+            message: "",
+            ignore_fields: vec![],
+            getters: true,
+            getters_ignore_fields: vec![],
+            setters: true,
+            setters_ignore_fields: vec![],
+        }
+    }
 }
