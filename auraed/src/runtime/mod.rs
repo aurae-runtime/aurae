146ddbb73fcf4bdc26efb666b9150f7b3f4fab66
Kris Nóva <kris@nivenly.com>
2022-10-23 06:55:27 -0700

    Introduce cgroup system to run_executable
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- auraed/src/runtime/mod.rs
+++ auraed/src/runtime/mod.rs
@@ -40,9 +40,17 @@ use anyhow::Result;
 use cgroups_rs::cgroup_builder::CgroupBuilder;
 use cgroups_rs::*;
 use cgroups_rs::{CgroupPid, Controller};
+<<<<<<< HEAD
 use log::{debug, info};
 use std::process::Stdio;
 use tonic::{Request, Response, Status};
+=======
+use tonic::{Request, Response, Status};
+// use libcontainer::{
+//     container::builder::ContainerBuilder, syscall::syscall::create_syscall,
+// };
+// use std::path::PathBuf;
+>>>>>>> 1502f87 (Introduce cgroup system to run_executable)
 
 /// The server side implementation of the core runtime subsystem.
 ///
@@ -63,6 +71,7 @@ impl Core for CoreService {
         request: Request<Executable>,
     ) -> Result<Response<ExecutableStatus>, Status> {
         let r = request.into_inner();
+<<<<<<< HEAD
         // let rmeta = r.meta.expect("parsing request meta");
 
         // Build command to execute.
@@ -134,11 +143,94 @@ impl Core for CoreService {
                     exit_code: output.status.code().expect("Parse status code").to_string(),
                 };
                 Ok(Response::new(response))
+=======
+        let rmeta = r.meta.expect("expect meta");
+        let cmd = command_from_string(&r.command);
+        match cmd {
+            // Successful parse
+            Ok(mut cmd) => {
+                // Create the cgroup
+                let hierarchy = cgroups_rs::hierarchies::auto(); // v1/v2 cgroup switch automatically
+                let cgroup: Cgroup = CgroupBuilder::new(&rmeta.name)
+                    .cpu()
+                    .shares(10) // Use 10% of the CPU relative to other cgroups
+                    .done()
+                    .build(hierarchy);
+
+                // Spawn the command
+                let running = cmd.spawn();
+                match running {
+                    Ok(running) => {
+                        let pid = running.id();
+
+                        // Attach the running command to the cgroup
+                        let cpus: &cgroups_rs::cpu::CpuController =
+                            cgroup.controller_of().expect("cgroup controller");
+                        cpus.add_task(&CgroupPid::from(pid as u64))
+                            .expect("attaching to cgroup");
+
+                        // Wait for the command to terminate
+                        let output = running
+                            .wait_with_output()
+                            .expect("waiting process termination");
+
+                        // Return the result synchronously
+                        let meta = meta::AuraeMeta {
+                            name: r.command,
+                            message: "-".to_string(),
+                        };
+                        let proc = meta::ProcessMeta { pid: pid as i32 };
+                        let status = meta::Status::Complete as i32;
+                        let response = ExecutableStatus {
+                            meta: Some(meta),
+                            proc: Some(proc),
+                            status,
+                            stdout: String::from_utf8(output.stdout)
+                                .expect("reading stdout"),
+                            stderr: String::from_utf8(output.stderr)
+                                .expect("reading stderr"),
+                            exit_code: output.status.to_string(),
+                        };
+                        Ok(Response::new(response))
+                    }
+                    Err(e) => {
+                        let meta = meta::AuraeMeta {
+                            name: "-".to_string(),
+                            message: format!(
+                                "failed spawning process: {:?}",
+                                e
+                            ),
+                        };
+                        let proc = meta::ProcessMeta { pid: -1 };
+                        let status = meta::Status::Error as i32;
+                        let response = ExecutableStatus {
+                            meta: Some(meta),
+                            proc: Some(proc),
+                            status,
+                            stdout: "-".to_string(),
+                            stderr: "-".to_string(),
+                            exit_code: "-".to_string(),
+                        };
+                        Ok(Response::new(response))
+                    }
+                }
+
+                // Get the process metadata
+
+                // Wait for the command (synchronous)
+
+                // Destroy the cgroup
+>>>>>>> 1502f87 (Introduce cgroup system to run_executable)
             }
+            // Unsuccessful parse
             Err(e) => {
                 let meta = meta::AuraeMeta {
                     name: "-".to_string(),
+<<<<<<< HEAD
                     message: format!("failed spawning process: {:?}", e),
+=======
+                    message: format!("unable to parse command: {:?}", e),
+>>>>>>> 1502f87 (Introduce cgroup system to run_executable)
                 };
                 let proc = meta::ProcessMeta { pid: -1 };
                 let status = meta::Status::Error as i32;
