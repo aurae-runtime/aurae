e507e2b4976ce8ed23d0ef35deb774d157ba4769
Dominic Hamon <dominic+github@google.com>
2022-11-22 10:03:57 +0000

    enable unused_results everywhere

--- auraescript_macros/src/lib.rs
+++ auraescript_macros/src/lib.rs
@@ -42,14 +42,14 @@
     while_true
 )]
 #![warn(missing_debug_implementations,
-        // TODO: missing_docs,
-        trivial_casts,
-        trivial_numeric_casts,
-        unused_extern_crates,
-        unused_import_braces,
-        unused_qualifications,
-        // TODO: unused_results
-        )]
+    // TODO: missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results
+)]
 
 use proc_macro::TokenStream;
 
