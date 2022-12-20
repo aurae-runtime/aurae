5f2ea715b2cd64d7b77f349280c9abf297fc9396
Dominic Hamon <dominic+github@google.com>
2022-12-20 16:02:34 +0000

    add journald tracing

--- auraed/src/init/logging.rs
+++ auraed/src/init/logging.rs
@@ -1,9 +1,18 @@
-use tracing::Level;
+use tracing::{info, Level};
+use tracing_subscriber::{prelude::*, util::TryInitError};
+
+const AURAED_SYSLOG_IDENT: &str = "auraed";
 
 #[derive(thiserror::Error, Debug)]
 pub(crate) enum LoggingError {
-    #[error("Failed to setup tracing: {source:?}")]
-    TracingSetupFailure { source: Box<dyn std::error::Error> },
+    #[error("Failed to setup basic tracing: {source:?}")]
+    SetupFailure { source: Box<dyn std::error::Error> },
+
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+
+    #[error(transparent)]
+    TryInitError(#[from] TryInitError),
 }
 
 pub(crate) fn init(logger_level: Level) -> Result<(), LoggingError> {
@@ -14,13 +23,17 @@ pub(crate) fn init(logger_level: Level) -> Result<(), LoggingError> {
 }
 
 fn init_syslog_logging(tracing_level: Level) -> Result<(), LoggingError> {
-    // TODO: multiple subscribers
-    // TODO: journald
+    info!("initializing syslog logging");
+    let journald_layer = tracing_journald::layer()?
+        .with_syslog_identifier(AURAED_SYSLOG_IDENT.into());
+
     tracing_subscriber::fmt()
         .compact()
         .with_env_filter(format!("auraed={tracing_level}"))
+        .finish()
+        .with(journald_layer)
         .try_init()
-        .map_err(|e| LoggingError::TracingSetupFailure { source: e })
+        .map_err(|e| e.into())
 }
 
 // To discuss here https://github.com/aurae-runtime/auraed/issues/24:
@@ -31,9 +44,10 @@ fn init_syslog_logging(tracing_level: Level) -> Result<(), LoggingError> {
 //      For now, auraed distinguishes between pid1 system and local (dev environment) logging.
 //      [1] https://docs.rs/syslog/latest/src/syslog/lib.rs.html#232-243
 fn init_pid1_logging(tracing_level: Level) -> Result<(), LoggingError> {
+    info!("initializing pid1 logging");
     tracing_subscriber::fmt()
         .compact()
         .with_env_filter(format!("auraed={tracing_level}"))
         .try_init()
-        .map_err(|e| LoggingError::TracingSetupFailure { source: e })
+        .map_err(|e| LoggingError::SetupFailure { source: e })
 }
