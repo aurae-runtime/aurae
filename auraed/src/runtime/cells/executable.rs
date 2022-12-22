78e22a8e925a43824b73edba53b6b7b597d11797
Future Highway <113635015+future-highway@users.noreply.github.com>
2022-12-22 15:12:10 -0500

    Call unshare in Executable's pre_exec

--- auraed/src/runtime/cells/executable.rs
+++ auraed/src/runtime/cells/executable.rs
@@ -43,56 +43,12 @@ impl Executable {
                 let mut command = Command::new("/usr/bin/sh");
                 let command = command.args(&["-c", &self.command]);
 
-                // // Namespaces
-                // //
-                // // TODO We need to track the namespace for all newly
-                // //      unshared namespaces within a Cell such that
-                // //      we can call command.set_namespace() for
-                // //      each of the new namespaces at the cell level!
-                // //      This will likely require changing how Cells
-                // //      manage namespaces as we need to cache the namespace
-                // //      IDs (names?)
-                // //
-                // // TODO Basically once a namespace has been created for a Cell
-                // //      we should put ALL future executables into the same namespace!
-                // let mut namespaces_to_unshare: Vec<Namespace> = vec![];
-                // // Note: The logic here is reversed. We define the flags as "share'
-                // //       and map them to "unshare".
-                // //       This is by design as the API has a concept of "share".
-                // if !spec.ns_share_mount {
-                //     info!("Unshare: mount");
-                //     namespaces_to_unshare.push(Namespace::Mount)
-                // }
-                // if !spec.ns_share_uts {
-                //     info!("Unshare: uts");
-                //     namespaces_to_unshare.push(Namespace::Uts)
-                // }
-                // if !spec.ns_share_ipc {
-                //     info!("Unshare: ipc");
-                //     namespaces_to_unshare.push(Namespace::Ipc)
-                // }
-                // if !spec.ns_share_pid {
-                //     info!("Unshare: pid");
-                //     namespaces_to_unshare.push(Namespace::Pid)
-                // }
-                // if !spec.ns_share_net {
-                //     info!("Unshare: net");
-                //     namespaces_to_unshare.push(Namespace::Net)
-                // }
-                // if !spec.ns_share_cgroup {
-                //     info!("Unshare: cgroup");
-                //     namespaces_to_unshare.push(Namespace::Cgroup)
-                // }
-                //
-                // let command = command.unshare(&namespaces_to_unshare);
-
                 // Run 'pre_exec' hooks from the context of the soon-to-be launched child.
                 let command = {
                     let executable_name = self.name.clone();
                     unsafe {
-                        command.pre_exec(move || {
-                            pre_exec(&executable_name, spec.clone())
-                        })
+                        command
+                            .pre_exec(move || pre_exec(&executable_name, &spec))
                     }
                 };
 
@@ -138,7 +94,7 @@ impl Executable {
 /// Common functionality within the context of the new executable
 fn pre_exec(
     executable_name: &ExecutableName,
-    spec: ValidatedCell,
+    spec: &ValidatedCell,
 ) -> io::Result<()> {
     info!("CellService: pre_exec(): {executable_name}");
     // Here we are executing as the new spawned pid.
@@ -146,12 +102,88 @@ fn pre_exec(
     // started with Aurae in the future. Similar to kprobe/uprobe
     // in Linux or LD_PRELOAD in libc.
 
+    pre_exec_unshare(executable_name, spec)?;
+
     // In the event we are not sharing the mount namespace or the pid namespace
     // with the host, we will manually mount /proc
     if !spec.ns_share_pid && !spec.ns_share_mount {
         info!("CellService: pre_exec(): mounting procfs /proc");
         // TODO Implement mount proc
         // TODO validate this logic is the correct logic for mounting proc in our new namespace isolation zone
+    }
+
+    Ok(())
+}
+
+// Namespaces
+//
+// TODO We need to track the namespace for all newly
+//      unshared namespaces within a Cell such that
+//      we can call command.set_namespace() for
+//      each of the new namespaces at the cell level!
+//      This will likely require changing how Cells
+//      manage namespaces as we need to cache the namespace
+//      IDs (names?)
+//
+// TODO Basically once a namespace has been created for a Cell
+//      we should put ALL future executables into the same namespace!
+fn pre_exec_unshare(
+    executable_name: &ExecutableName,
+    spec: &ValidatedCell,
+) -> io::Result<()> {
+    info!("CellService: pre_exec_unshare(): {executable_name}");
+
+    // Note: The logic here is reversed. We define the flags as "share'
+    //       and map them to "unshare".
+    //       This is by design as the API has a concept of "share".
+    if !spec.ns_share_mount {
+        info!("Unshare: mount");
+        if let Err(err_no) =
+            nix::sched::unshare(nix::sched::CloneFlags::CLONE_NEWNS)
+        {
+            return Err(io::Error::from_raw_os_error(err_no as i32));
+        }
+    }
+    if !spec.ns_share_uts {
+        info!("Unshare: uts");
+        if let Err(err_no) =
+            nix::sched::unshare(nix::sched::CloneFlags::CLONE_NEWUTS)
+        {
+            return Err(io::Error::from_raw_os_error(err_no as i32));
+        }
+    }
+    if !spec.ns_share_ipc {
+        info!("Unshare: ipc");
+        if let Err(err_no) =
+            nix::sched::unshare(nix::sched::CloneFlags::CLONE_NEWIPC)
+        {
+            return Err(io::Error::from_raw_os_error(err_no as i32));
+        }
+    }
+    if !spec.ns_share_pid {
+        info!("Unshare: pid");
+        if let Err(err_no) =
+            nix::sched::unshare(nix::sched::CloneFlags::CLONE_NEWPID)
+        {
+            return Err(io::Error::from_raw_os_error(err_no as i32));
+        }
+    }
+    if !spec.ns_share_net {
+        info!("Unshare: net");
+        if let Err(err_no) =
+            nix::sched::unshare(nix::sched::CloneFlags::CLONE_NEWNET)
+        {
+            return Err(io::Error::from_raw_os_error(err_no as i32));
+        }
+    }
+
+    if !spec.ns_share_cgroup {
+        info!("Unshare: cgroup");
+        if let Err(err_no) =
+            nix::sched::unshare(nix::sched::CloneFlags::CLONE_NEWCGROUP)
+        {
+            return Err(io::Error::from_raw_os_error(err_no as i32));
+        }
     }
 
     Ok(())
