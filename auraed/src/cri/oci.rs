21a88828bc29ce15bfc13a71652f4728d051cdaf
Kris Nóva <kris@nivenly.com>
2023-01-29 11:51:34 -0800

    Introduce mounts to oci defaults
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- auraed/src/cri/oci.rs
+++ auraed/src/cri/oci.rs
@@ -29,7 +29,7 @@
 \* -------------------------------------------------------------------------- */
 
 use aurae_proto::cri::PodSandboxConfig;
-use oci_spec::runtime::{Spec, SpecBuilder};
+use oci_spec::runtime::{MountBuilder, RootBuilder, Spec, SpecBuilder};
 use oci_spec::OciSpecError;
 
 pub struct AuraeOCIBuilder {
@@ -40,7 +40,98 @@ impl AuraeOCIBuilder {
     pub fn new() -> AuraeOCIBuilder {
         AuraeOCIBuilder {
             // TODO Port config.json to this builder
-            spec_builder: SpecBuilder::default().version("1.0.2-dev"),
+            spec_builder: SpecBuilder::default()
+                .version("1.0.2-dev")
+                .root(
+                    RootBuilder::default()
+                        .path("rootfs")
+                        .readonly(false)
+                        .build()
+                        .expect("default oci: root"),
+                )
+                .mounts(vec![
+                    MountBuilder::default()
+                        .destination("/proc")
+                        .typ("proc")
+                        .source("proc")
+                        .build()
+                        .expect("default oci: mount /proc"),
+                    MountBuilder::default()
+                        .destination("/dev")
+                        .typ("devtmpfs")
+                        .source("devtmpfs")
+                        .options(vec![
+                            "nosuid".to_string(),
+                            "strictatime".to_string(),
+                            "mode=755".to_string(),
+                            "size=65536k".to_string(),
+                        ])
+                        .build()
+                        .expect("default oci: mount /dev"),
+                    MountBuilder::default()
+                        .destination("/dev/pts")
+                        .typ("devpts")
+                        .source("devpts")
+                        .options(vec![
+                            "nosuid".to_string(),
+                            "noexec".to_string(),
+                            "newinstance".to_string(),
+                            "ptmxmode=0666".to_string(),
+                            "mode=0620".to_string(),
+                            "gid=5".to_string(),
+                        ])
+                        .build()
+                        .expect("default oci: mount /dev/pts"),
+                    MountBuilder::default()
+                        .destination("/dev/shm")
+                        .typ("tmpfs")
+                        .source("shm")
+                        .options(vec![
+                            "nosuid".to_string(),
+                            "noexec".to_string(),
+                            "nodev".to_string(),
+                            "mode=1777".to_string(),
+                            "size=65536k".to_string(),
+                        ])
+                        .build()
+                        .expect("default oci: mount /dev/shm"),
+                    MountBuilder::default()
+                        .destination("/dev/mqueue")
+                        .typ("mqueue")
+                        .source("mqueue")
+                        .options(vec![
+                            "nosuid".to_string(),
+                            "noexec".to_string(),
+                            "nodev".to_string(),
+                        ])
+                        .build()
+                        .expect("default oci: mount /dev/shm"),
+                    MountBuilder::default()
+                        .destination("/sys")
+                        .typ("sysfs")
+                        .source("sysfs")
+                        .options(vec![
+                            "nosuid".to_string(),
+                            "noexec".to_string(),
+                            "nodev".to_string(),
+                            "ro".to_string(),
+                        ])
+                        .build()
+                        .expect("default oci: mount /sys"),
+                    MountBuilder::default()
+                        .destination("/sys/fs/cgroup")
+                        .typ("cgroup")
+                        .source("cgroup")
+                        .options(vec![
+                            "nosuid".to_string(),
+                            "noexec".to_string(),
+                            "nodev".to_string(),
+                            "relatime".to_string(),
+                            "ro".to_string(),
+                        ])
+                        .build()
+                        .expect("default oci: mount /sys/fs/cgroup"),
+                ]),
         }
     }
     pub fn overload_pod_sandbox_config(
@@ -55,3 +146,104 @@ impl AuraeOCIBuilder {
         self.spec_builder.build()
     }
 }
+
+//   "process": {
+//     "terminal": false,
+//     "user": {
+//       "uid": 0,
+//       "gid": 0
+//     },
+//     "args": [
+//       "init"
+//     ],
+//     "env": [
+//       "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+//       "TERM=xterm"
+//     ],
+//     "cwd": "/",
+//     "capabilities": {
+//       "bounding": [
+//         "CAP_AUDIT_WRITE",
+//         "CAP_NET_BIND_SERVICE",
+//         "CAP_KILL"
+//       ],
+//       "effective": [
+//         "CAP_AUDIT_WRITE",
+//         "CAP_NET_BIND_SERVICE",
+//         "CAP_KILL"
+//       ],
+//       "inheritable": [
+//         "CAP_AUDIT_WRITE",
+//         "CAP_NET_BIND_SERVICE",
+//         "CAP_KILL"
+//       ],
+//       "permitted": [
+//         "CAP_AUDIT_WRITE",
+//         "CAP_NET_BIND_SERVICE",
+//         "CAP_KILL"
+//       ],
+//       "ambient": [
+//         "CAP_AUDIT_WRITE",
+//         "CAP_NET_BIND_SERVICE",
+//         "CAP_KILL"
+//       ]
+//     },
+//     "rlimits": [
+//       {
+//         "type": "RLIMIT_NOFILE",
+//         "hard": 1024,
+//         "soft": 1024
+//       }
+//     ],
+//     "noNewPrivileges": true
+//   },
+//   "hostname": "aurae",
+//   "annotations": {},
+//   "linux": {
+//     "resources": {
+//       "devices": [
+//         {
+//           "allow": false,
+//           "type": null,
+//           "major": null,
+//           "minor": null,
+//           "access": "rwm"
+//         }
+//       ]
+//     },
+//     "namespaces": [
+//       {
+//         "type": "pid"
+//       },
+//       {
+//         "type": "network"
+//       },
+//       {
+//         "type": "ipc"
+//       },
+//       {
+//         "type": "uts"
+//       },
+//       {
+//         "type": "mount"
+//       }
+//     ],
+//     "maskedPaths": [
+//       "/proc/acpi",
+//       "/proc/asound",
+//       "/proc/kcore",
+//       "/proc/keys",
+//       "/proc/latency_stats",
+//       "/proc/timer_list",
+//       "/sys/firmware",
+//       "/proc/scsi"
+//     ],
+//     "readonlyPaths": [
+//       "/proc/bus",
+//       "/proc/fs",
+//       "/proc/irq",
+//       "/proc/sys",
+//       "/proc/sysrq-trigger"
+//     ]
+//   }
+// }
