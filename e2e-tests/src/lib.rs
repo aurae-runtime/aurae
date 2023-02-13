e96bec318826cf45a9524ab21ed7677ebb98b653
Jeroen Soeters <wfhsoeters@gmail.com>
2023-02-13 22:49:11 +0000

    Setup lints for e2e tests

--- e2e-tests/src/lib.rs
+++ e2e-tests/src/lib.rs
@@ -1,1 +1,22 @@
+// Lint groups: https://doc.rust-lang.org/rustc/lints/groups.html
+#![warn(future_incompatible, nonstandard_style, unused)]
+#![warn(
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    unconditional_recursion,
+    unused_comparisons,
+    while_true
+)]
+#![warn(
+    missing_debug_implementations,
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_results
+)]
+#![warn(clippy::unwrap_used)]
+
 mod observe;
