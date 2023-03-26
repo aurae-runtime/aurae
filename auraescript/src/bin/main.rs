69167ca4c4f09a9dfb54fb9b35ad286226c2c2bd
sean watters <sean@watters.io>
2023-03-26 10:17:07 -0700

    feat(auraescript) converts from deno_core::JsRuntime to deno_runtime::MainWorker (#466)
    * transitions AuraeScript from deno_core::JsRuntime to deno_runtime::MainWorker
    
    * upgrades uuid from 1.1.2 to 1.2.2
    
    * resolves issues with deno_ast upgrade
    
    * changed default params to empty strings

--- auraescript/src/bin/main.rs
+++ auraescript/src/bin/main.rs
@@ -62,17 +62,15 @@ fn main() -> anyhow::Result<()> {
         std::process::exit(1);
     }
 
-    let mut js_runtime = init();
-
-    let _ = js_runtime.execute_script("", "Deno.core.initializeAsyncOps();")?;
-
     let main_module = resolve_path(&args[1].clone(), current_dir()?.as_path())?;
+    let mut main_worker = init(main_module.clone());
+
+    let _ =
+        main_worker.execute_script("", "Deno.core.initializeAsyncOps();")?;
 
     let future = async move {
-        let mod_id = js_runtime.load_main_module(&main_module, None).await?;
-        let result = js_runtime.mod_evaluate(mod_id);
-        js_runtime.run_event_loop(false).await?;
-        result.await?
+        main_worker.execute_main_module(&main_module).await?;
+        main_worker.run_event_loop(false).await
     };
 
     tokio::runtime::Builder::new_current_thread()
