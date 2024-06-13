0c1004d08eb047b06a74cdfdb455cf8766b1dd48
jan0ski <mccormickt9@gmail.com>
2024-06-13 02:14:13 -0400

    feat(vm): add initial implementation for managing cloudhypervisor vms

---
+++ auraed/src/vms/manager.rs
@@ -0,0 +1,68 @@
+use std::sync::{
+    mpsc::{channel, Sender},
+    Arc,
+};
+
+use hypervisor::Hypervisor;
+use libc::EFD_NONBLOCK;
+use vmm::{api::ApiRequest, VmmThreadHandle};
+use vmm_sys_util::eventfd::EventFd;
+
+pub struct Manager {
+    pub events: EventFd,
+    pub sender: Option<Sender<ApiRequest>>,
+    hypervisor: Arc<dyn Hypervisor>,
+    exit: EventFd,
+    monitor: EventFd,
+    debug: EventFd,
+    vmm_thread: Option<VmmThreadHandle>,
+}
+
+impl Manager {
+    pub fn new() -> Self {
+        let exit =
+            EventFd::new(EFD_NONBLOCK).expect("Failed to create exit eventfd");
+        let monitor =
+            EventFd::new(EFD_NONBLOCK).expect("Failed to create event monitor");
+        let debug =
+            EventFd::new(EFD_NONBLOCK).expect("Failed to create event monitor");
+        let api_evt =
+            EventFd::new(EFD_NONBLOCK).expect("Failed to create API eventfd");
+
+        let hypervisor =
+            hypervisor::new().expect("Failed to instantiate hypervisor");
+
+        Self {
+            hypervisor,
+            exit,
+            monitor,
+            debug,
+            sender: None,
+            events: api_evt,
+            vmm_thread: None,
+        }
+    }
+
+    pub fn start(&mut self) -> Result<(), anyhow::Error> {
+        let (sender, receiver) = channel();
+        self.sender = Some(sender.clone());
+
+        let version =
+            vmm::VmmVersionInfo::new("auraed", env!("CARGO_PKG_VERSION"));
+        self.vmm_thread = Some(
+            vmm::start_vmm_thread(
+                version,
+                &None,
+                None,
+                self.events.try_clone()?,
+                sender,
+                receiver,
+                self.debug.try_clone()?,
+                &seccompiler::SeccompAction::Allow,
+                self.hypervisor.clone(),
+            )
+            .expect("Failed to start VMM thread"),
+        );
+        Ok(())
+    }
+}
--- auraed/src/vms/mod.rs
+++ auraed/src/vms/mod.rs
@@ -1,1 +1,4 @@
+pub mod manager;
+pub mod virtual_machine;
+pub mod virtual_machines;
 pub mod vm_service;
