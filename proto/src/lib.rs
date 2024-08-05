55c9ed229042388bbc27e9dfb268971e429f78ab
Chris Mucciolo <mooch@render-se.com>
2024-08-05 15:12:48 +0000

    allow clippy::doc_lazy_continuation in proto crate

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -33,6 +33,7 @@
 #![allow(clippy::derive_partial_eq_without_eq)]
 #![allow(clippy::empty_docs)]
 #![allow(clippy::match_single_binding)]
+#![allow(clippy::doc_lazy_continuation)]
 
 pub mod cells {
     include!("../gen/aurae.cells.v0.rs");
