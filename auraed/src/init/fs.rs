1d62cb044fc973b3b8006466c31ef3f35ecbca63
Tommy McCormick <16668493+mccormickt@users.noreply.github.com>
2024-11-10 14:33:45 -0500

    Fix: Setup cgroupfs to run cells when running as PID1 (#533)
    * fix: setup cgroupfs for running cells as pid1
    
    * chore: remove unneeded deps
    
    * fix: trait function name collisions
    
    * fix(tests): set default executable to never-ending command

--- auraed/src/init/fs.rs
+++ auraed/src/init/fs.rs
@@ -13,7 +13,8 @@
  * SPDX-License-Identifier: Apache-2.0                                        *
 \* -------------------------------------------------------------------------- */
 
-use nix::mount::MsFlags;
+use lazy_static::lazy_static;
+use nix::{mount::MsFlags, sys::stat::Mode};
 use std::io;
 use tracing::{error, info};
 
@@ -21,6 +22,30 @@ use tracing::{error, info};
 pub(crate) enum FsError {
     #[error("Failed to mount {spec:?} due to error: {source}")]
     MountFailure { spec: MountSpec, source: io::Error },
+    #[error(transparent)]
+    FileCreationFailure(#[from] nix::errno::Errno),
+}
+
+lazy_static! {
+    pub static ref CHMOD_0755: Mode = Mode::S_IRWXU
+        | Mode::S_IRGRP
+        | Mode::S_IXGRP
+        | Mode::S_IROTH
+        | Mode::S_IXOTH;
+    pub static ref CHMOD_0555: Mode = Mode::S_IRUSR
+        | Mode::S_IXUSR
+        | Mode::S_IRGRP
+        | Mode::S_IXGRP
+        | Mode::S_IROTH
+        | Mode::S_IXOTH;
+    pub static ref CHMOD_1777: Mode =
+        Mode::S_IRWXU | Mode::S_IRWXG | Mode::S_IRWXO | Mode::S_ISVTX;
+    pub static ref COMMON_MNT_FLAGS: MsFlags =
+        MsFlags::MS_NODEV | MsFlags::MS_NOEXEC | MsFlags::MS_NOSUID;
+    pub static ref CGROUP_MNT_FLAGS: MsFlags = MsFlags::MS_NODEV
+        | MsFlags::MS_NOEXEC
+        | MsFlags::MS_NOSUID
+        | MsFlags::MS_RELATIME;
 }
 
 #[derive(Debug)]
@@ -28,6 +53,8 @@ pub(crate) struct MountSpec {
     pub source: Option<&'static str>,
     pub target: &'static str,
     pub fstype: Option<&'static str>,
+    pub flags: MsFlags,
+    pub data: Option<&'static str>,
 }
 
 impl MountSpec {
@@ -38,8 +65,8 @@ impl MountSpec {
             self.source,
             self.target,
             self.fstype,
-            MsFlags::empty(),
-            None::<&str>,
+            self.flags,
+            self.data,
         ) {
             error!("Failed to mount {:?}", self);
             return Err(FsError::MountFailure {
